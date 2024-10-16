c0abaf585fa6a61272648e482b6e7f62f7a4fbfb
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-10-16 11:19:11 -0500

    fix abilitySevice

--- src/main/java/com/techQuest/TechQuest/services/auth/AuthService.java
+++ src/main/java/com/techQuest/TechQuest/services/auth/AuthService.java
@@ -1,11 +1,6 @@
 package com.techQuest.TechQuest.services.auth;
 
-import com.riwi.hero_training.application.dtos.requests.AuthUserRequestDto;
-import com.riwi.hero_training.application.dtos.responses.AuthUserResponseDto;
-import com.riwi.hero_training.domain.entities.UserEntity;
-import com.riwi.hero_training.domain.exceptions.InvalidCredentialException;
-import com.riwi.hero_training.domain.ports.service.interfaces.IAuthService;
-import com.riwi.hero_training.infrastructure.helpers.JwtUtil;
+
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -32,18 +27,16 @@ public class AuthService implements IAuthService {
 
     @Override
     public AuthUserResponseDto login(AuthUserRequestDto request) {
-        // Cargar el usuario a partir del identificador (nombre de usuario o correo)
-        UserEntity user = (UserEntity) userDetailsService.loadUserByUsername(request.getIdentifier());
+        UserEntity user = (UserEntity) userDetailsService.loadUserByUsername(request.getIdentifier()
 
-        //Verifica si las contraseña coinciden
         if(!passwordEncoder.matches(request.getPassword(), user.getPassword())){
             throw  new InvalidCredentialException("Invalid credential");
         }
 
-        //Realiza la autentificacion
+        //autenticar
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getIdentifier(), request.getPassword()));
 
-        //Genera la respuesta de autentificacion del token JWT
+        //jwt response
         return AuthUserResponseDto.builder()
                 .message(user.getRole() + "Succesfull authentication") // Mensajito
                 .token(this.jwtUtil.generateToken(user))// Genera el token
