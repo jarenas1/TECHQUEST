45eabefd6ad107ecb78cd99b25fee05918b774ce
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-10-16 11:26:24 -0500

    create Controller Structure

---
+++ src/main/java/com/techQuest/TechQuest/controller/AbilityController.java
@@ -0,0 +1,9 @@
+package com.techQuest.TechQuest.controller;
+
+import com.techQuest.TechQuest.services.AbilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AbilityController {
+    @Autowired
+    private AbilityService abilityService;
+}
---
+++ src/main/java/com/techQuest/TechQuest/controller/MissionController.java
@@ -0,0 +1,10 @@
+package com.techQuest.TechQuest.controller;
+
+import com.techQuest.TechQuest.services.MissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MissionController {
+
+    @Autowired
+    private MissionService missionService;
+}
