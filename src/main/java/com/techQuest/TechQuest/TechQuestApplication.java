d7cce746176fce67190945249d172035b59ee933
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-10-15 23:24:00 -0500

    initial commit

---
+++ src/main/java/com/techQuest/TechQuest/TechQuestApplication.java
@@ -0,0 +1,13 @@
+package com.techQuest.TechQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techQuest/TechQuest/TechQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techQuest.TechQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
